c305cbf56a0ea1f6a843adcc6f62dd38206ce1c7
Jordan Lawler <jordan.lawler@colorado.edu>
2016-02-03 02:05:12 -0700

    integrated with soundcloud API. Is taking in audio, fingerprinting 50 times a second, then pushing payload to server every 5 seconds.

---
+++ app/views/home/sketchmic.js
@@ -0,0 +1,86 @@
+<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/237811570&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+<script type="text/javascript">
+
+var mic, fft;
+function setup(){
+  createCanvas(1280, 800);
+  mic = new p5.AudioIn()
+  mic.start();
+  fft = new p5.FFT();
+  fft.setInput(mic);
+  botPeak = 0;
+  botHighPeak = 0;
+  midPeak = 0;
+  midHighPeak = 0;
+  highPeak = 0;
+  // amp = new p5.Amplitude();
+}
+
+var myVar = setInterval(myTimer, 1000);
+var array = []
+
+function myTimer() {
+  $.ajax({
+          type: 'POST',
+          url: '/songs',
+          data: { frequencies: array }
+      });
+  console.log(array);
+}
+
+function draw(){
+
+  var micLevel = mic.getLevel()
+  var ampLevel = map(micLevel, 0, 1, 0, .6)
+
+  var spectrum = fft.analyze();
+  var botFreq = fft.getEnergy(30, 40);
+  var botHighFreq = fft.getEnergy(40, 80);
+  var midFreq = fft.getEnergy(80, 120);
+  var midHighFreq = fft.getEnergy(120, 180);
+  var highFreq = fft.getEnergy(180, 300);
+  //
+  for (var i = 0; i< spectrum.length; i++){
+  }
+
+  if (botPeak < botFreq){
+      botPeak = botFreq;
+
+      array = [botPeak, botHighPeak, midPeak, midHighPeak]
+      // console.log(array);
+
+  }
+
+  if (botHighPeak < botHighFreq){
+      botHighPeak = botHighFreq;
+
+      array = [botPeak, botHighPeak, midPeak, midHighPeak, highPeak]
+      // console.log(array);
+  }
+
+  if (midPeak < midFreq){
+      midPeak = midFreq;
+
+      array = [botPeak, botHighPeak, midPeak, midHighPeak, highPeak]
+      // console.log(array);
+  }
+
+  if (midHighPeak < midHighFreq){
+      midHighPeak = midHighFreq;
+
+      array = [botPeak, botHighPeak, midPeak, midHighPeak, highPeak]
+      // console.log(array);
+  }
+
+  if (highPeak < highFreq){
+      highPeak = highFreq;
+
+      array = [botPeak, botHighPeak, midPeak, midHighPeak, highPeak]
+      // console.log(array);
+  }
+
+  endShape();
+
+}
+
+</script>
